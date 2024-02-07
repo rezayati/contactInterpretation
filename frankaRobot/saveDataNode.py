3cd6dbd4ea52616d4f5007d3291e4e55ae5285ab
Maryam REZAYATI <maryam.rezayati@yahoo.com>
2024-02-07 09:35:32 +0100

    Update saveDataNode.py

--- frankaRobot/saveDataNode.py
+++ frankaRobot/saveDataNode.py
@@ -16,8 +16,7 @@ How to Run:
 
 2. Connecting to the Robot (Running Frankapy):
     - Open a terminal.
-    - Navigate to /home/mindlab/franka.
-    - Run the bash script: bash run_frankapy.sh.
+    - Run the bash script: bash /home/mindlab/franka/run_frankapy.sh.
 
 3. Specify the Output Folder (PATH) - (line 56 in the code).
 
