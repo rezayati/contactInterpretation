12863b11001965ffa595b71704bd9d358cfcf1b1
Maryam REZAYATI <maryam.rezayati@yahoo.com>
2024-02-07 09:40:54 +0100

    Update main.py

--- frankaRobot/main.py
+++ frankaRobot/main.py
@@ -2,47 +2,37 @@
 """
 By Maryam Rezayati
 
-How to run?
-## 1st  Step: unlock robot
-	1) connect to the robot desk with the ID (172.16.0.2 or 192.168.15.33)
-	2) unlock the robot and activate FCI
+# How to run?
+1. unlock robot
+	-connect to the robot desk with the ID (172.16.0.2 or 192.168.15.33)
+	-unlock the robot and activate FCI
 
-## 2nd Step: run frankapy
+2. run frankapy
+	-open an terminal
+		conda activate frankapyenv
+		bash /home/mindlab/franka/run_frankapy.sh
 
-open an terminal
+3. run digital glove node
+	-open another temrinal
+		source /opt/ros/noetic/setup.bash
+		/home/mindlab/miniconda3/envs/frankapyenv/bin/python /home/mindlab/contactInterpretation/dataLabeling/digitalGloveNode.py
 
-	conda activate frankapyenv
-	bash /home/mindlab/franka/run_frankapy.sh
+4. run robot node
+	-open another terminal 
+		conda activate frankapyenv
+		source /opt/ros/noetic/setup.bash
+		source /home/mindlab/franka/franka-interface/catkin_ws/devel/setup.bash --extend
+		source /home/mindlab/franka/frankapy/catkin_ws/devel/setup.bash --extend
+	
+		/home/mindlab/miniconda3/envs/frankapyenv/bin/python3 /home/mindlab/contactInterpretation/frankRobot/main.py
 
-## 3nd Step: run digital glove node
-
-open another temrinal
-
-	source /opt/ros/noetic/setup.bash
-	/home/mindlab/miniconda3/envs/frankapyenv/bin/python /home/mindlab/contactInterpretation/dataLabeling/digitalGloveNode.py
-
-## 4th Step: run robot node
-
-open another terminal 
-
-	conda activate frankapyenv
-	source /opt/ros/noetic/setup.bash
-	source /home/mindlab/franka/franka-interface/catkin_ws/devel/setup.bash --extend
-	source /home/mindlab/franka/frankapy/catkin_ws/devel/setup.bash --extend
-
-	/home/mindlab/miniconda3/envs/frankapyenv/bin/python3 /home/mindlab/contactInterpretation/frankRobot/main.py
-
-## 5th Step: run save data node
-
-open another terminal
-
-	source /opt/ros/noetic/setup.bash
-	source /home/mindlab/franka/franka-interface/catkin_ws/devel/setup.bash --extend
-	source /home/mindlab/franka/frankapy/catkin_ws/devel/setup.bash --extend
-
-	/home/mindlab/miniconda3/envs/frankapyenv/bin/python3 /home/mindlab/contactInterpretation/frankaRobot/saveDataNode.py
-
-
+5. run save data node
+	-open another terminal
+		source /opt/ros/noetic/setup.bash
+		source /home/mindlab/franka/franka-interface/catkin_ws/devel/setup.bash --extend
+		source /home/mindlab/franka/frankapy/catkin_ws/devel/setup.bash --extend
+	
+		/home/mindlab/miniconda3/envs/frankapyenv/bin/python3 /home/mindlab/contactInterpretation/frankaRobot/saveDataNode.py
 
 # to chage publish rate of frankastate go to : 
 sudo nano /home/mindlab/franka/franka-interface/catkin_ws/src/franka_ros_interface/launch/franka_ros_interface.launch
